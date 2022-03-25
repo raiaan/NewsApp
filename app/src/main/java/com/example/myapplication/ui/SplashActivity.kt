a98382c52abd184c77e4c4ff2ad940611a0f432a
esraakhaled <www.esraa.khaled.fciscusuez@gmail.com>
2022-03-25 13:21:31 +0200

    splash screen and icon for launcher screen

---
+++ app/src/main/java/com/example/myapplication/ui/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.MainActivity
+import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivitySplashBinding
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       news.alpha = 0f
+        news.animate().setDuration(3000).alpha(1f).withEndAction {
+            var intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
