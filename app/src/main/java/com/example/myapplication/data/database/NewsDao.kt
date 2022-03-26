2c5a21ffac4f50429785c0c8445d4d4d7cbac42a
esraakhaled <www.esraa.khaled.fciscusuez@gmail.com>
2022-03-26 21:27:41 +0200

    reformat code

--- app/src/main/java/com/example/myapplication/data/database/NewsDao.kt
+++ app/src/main/java/com/example/myapplication/data/database/NewsDao.kt
@@ -5,14 +5,13 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.myapplication.data.models.Articles
-import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NewsDao {
     @Query("SELECT * FROM articles ORDER BY publishedAt ASC")
     fun getCachedArticles(): List<Articles>
 
-    @Insert (onConflict = OnConflictStrategy.REPLACE)
-    fun addArticle(article:Articles)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addArticle(article: Articles)
 
 }
--- app/src/main/java/com/example/myapplication/ui/home/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/viewModel/HomeViewModel.kt
@@ -1,42 +1,43 @@
 package com.example.myapplication.ui.home.viewModel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.example.myapplication.data.haveNetworkConnection
 import com.example.myapplication.data.models.Articles
-import com.example.myapplication.data.models.Json4Kotlin_Base
 import com.example.myapplication.data.models.Repository
 import kotlinx.coroutines.*
 
-class HomeViewModel constructor(private val mainRepository: Repository) : ViewModel(){
-    var articlesLiveData= MutableLiveData<List<Articles>?>()
+class HomeViewModel constructor(private val mainRepository: Repository) : ViewModel() {
+    var articlesLiveData = MutableLiveData<List<Articles>?>()
     val errorMessage = MutableLiveData<String>()
     private val loading = MutableLiveData<Boolean>()
     var job: Job? = null
-    fun getLatestNews(){
-       // if (haveNetworkConnection()){
-            job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
-                articlesLiveData.postValue(mainRepository.cacheResult())
-            }
-       // }
+    fun getLatestNews() {
+        // if (haveNetworkConnection()){
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            articlesLiveData.postValue(mainRepository.cacheResult())
+        }
+        // }
+    }
+
+    fun cacheResult() {
 
     }
 
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
         onError("Exception handled: ${throwable.localizedMessage}")
     }
+
     private fun onError(message: String) {
         errorMessage.postValue(message)
         loading.postValue(false)
     }
-    fun addToFavourite(articles: Articles){
+
+    fun addToFavourite(articles: Articles) {
         job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
             mainRepository.insertToFavourite(articles)
         }
     }
+
     override fun onCleared() {
         super.onCleared()
         job?.cancel()
